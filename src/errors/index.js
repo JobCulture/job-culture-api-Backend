eeae3949bd34798cafe4b6f06b61a83af2812578
AndrewKins <andrewm.enoe@gmail.com>
2022-08-06 22:57:50 -0400

    created router, backed server up and running

--- src/errors/index.js
+++
@@ -1,9 +0,0 @@
-export class RecordNotFoundError extends Error {}
-
-export class BusinessError extends Error {}
-
-export class ValidationError extends Error {
-    constructor(errors) {
-        this.errors = errors;
-    }
-}

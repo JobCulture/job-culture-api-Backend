37bb98dfc845fb4799c6037c54a7f9533892f00c
Deise Costa <deisecj@gmail.com>
2022-08-18 19:46:37 -0400

    Fix paths

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(cors())
 //All endpoints go here
-const router = require('../job-culture-api-Backend/routes/userrouter')
-const companyrouter = require('../job-culture-api-Backend/routes/companyrouter')
-const reviewrouter = require('../job-culture-api-Backend/routes/reviewrouter')
+const router = require('./routes/userrouter')
+const companyrouter = require('./routes/companyrouter')
+const reviewrouter = require('./routes/reviewrouter')
 app.use('/',router) //define endpoints here. //router function is called here
 app.use('/',companyrouter) //define company router here
 app.use('/',reviewrouter)//review router defined here

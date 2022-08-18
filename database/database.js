14328ec0d7c121dadf527f21eb4b339daa0b5c68
Deise Costa <deisecj@gmail.com>
2022-08-18 19:45:04 -0400

    Heroku set up

--- database/database.js
+++ database/database.js
@@ -1,8 +1,18 @@
 const { Pool } = require("pg");
-const pool = new Pool({
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE
-})
+
+const config = process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    : {
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE
+    };
+
+const pool = new Pool(config)
 
 module.exports = pool;

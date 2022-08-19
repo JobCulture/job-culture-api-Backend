4c446e398439f357f7d438b3ffe5794f25d8382f
Deise Costa <deisecj@gmail.com>
2022-08-19 12:58:52 -0400

    create characteristic route

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 3000
-app.use(express.json())
-app.use(cors())
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(express.json());
+app.use(cors());
+
 //All endpoints go here
-const router = require('./routes/userrouter')
-const companyrouter = require('./routes/companyrouter')
-const reviewrouter = require('./routes/reviewrouter')
-app.use('/',router) //define endpoints here. //router function is called here
-app.use('/',companyrouter) //define company router here
-app.use('/',reviewrouter)//review router defined here
+const router = require('./routes/userrouter');
+const companyrouter = require('./routes/companyrouter');
+const reviewrouter = require('./routes/reviewrouter');
+const characteristicrouter = require('./routes/characteristicrouter');
+
+app.use('/', router) //define endpoints here. //router function is called here
+app.use('/', companyrouter) //define company router here
+app.use('/', reviewrouter)//review router defined here
+app.use('/', characteristicrouter); //characteristic router defined here
 app.get('/', (req, res) => {
     res.status(200).json({"message" : "This Api Is Working :)"});
 })
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
-  })
+  });
